bac0fb6c9f7dae46200a480168f31053281a07f7
Paul Liverman <paul.liverman.iii@gmail.com>
2015-04-11 01:23:58 -0700

    init

---
+++ src/conf.lua
@@ -0,0 +1,21 @@
+local debug = true
+
+function love.conf(t)
+	t.identity = "bsg"
+	t.version = "0.9.1"
+	if debug then t.console = true end
+
+	t.window = {}
+	t.window.title = "Battlestar Galactica, 33"
+	t.window.width = 960
+	t.window.height = 540
+	t.window.fullscreen = false
+	--t.window.borderless = true
+	t.window.resizable = false
+
+	--t.modules = {}
+	t.modules.joystick = false
+	t.modules.physics = false
+end
+
+return debug
